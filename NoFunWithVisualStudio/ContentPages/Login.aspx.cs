dc51c64d1bf795761f23fb80ff51e46c5b212f64
Chi Zhang <czh4@iu.edu>
2024-04-13 15:20:52 -0400

    included classes in Login.aspx.cs

--- NoFunWithVisualStudio/ContentPages/Login.aspx.cs
+++ NoFunWithVisualStudio/ContentPages/Login.aspx.cs
@@ -5,6 +5,13 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using System.Web.Security;
+using System.Configuration;
+using System.Data;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
 namespace NoFunWithVisualStudio.ContentPages
 {
     public partial class Login : System.Web.UI.Page
