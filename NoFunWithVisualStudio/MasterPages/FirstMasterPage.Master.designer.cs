1c0e363e874b1ded7fa291dc397cf7e61319d144
Chi Zhang <czh4@iu.edu>
2024-04-13 23:43:55 -0400

    version before moving all OLD files

--- NoFunWithVisualStudio/MasterPages/FirstMasterPage.Master.designer.cs
+++ NoFunWithVisualStudio/MasterPages/FirstMasterPage.Master.designer.cs
@@ -33,6 +33,24 @@ namespace NoFunWithVisualStudio.MasterPages
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// MainSiteMenu control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Menu MainSiteMenu;
+
+        /// <summary>
+        /// SiteMapDataSource1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SiteMapDataSource SiteMapDataSource1;
+
+        /// <summary>
         /// cpMainContent control.
         /// </summary>
         /// <remarks>
